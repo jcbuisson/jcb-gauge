8810606be6af4268388116a3109b91aa6dfd25f9
JC BUISSON <buisson@enseeiht.fr>
2024-01-27 06:06:34 +0100

    3.1.0: added subtitle

--- src/jcb-gauge.js
+++ src/jcb-gauge.js
@@ -5,7 +5,7 @@ const R = 2000
 
 /**
    * A custom-element which displays a gauge
-   * @attr {String} name - The text to display on the lower part of the gauge
+   * @attr {String} title - The text to display on the lower part of the gauge
    * @attr {Object} domain - { inf, sup }
    * @attr {Object} norm - { d1, d2, dt1, dt2 }
    * @attr {Object} value - { d1, d2, dt1, dt2 }
@@ -20,7 +20,8 @@ export class Gauge extends LitElement {
 
    static get properties() {
       return {
-         name: { type: String },
+         title: { type: String },
+         subtitle: { type: String },
          domain: {type: Object},
          norm: { type: Object },
          value: { type: Object },
@@ -30,7 +31,8 @@ export class Gauge extends LitElement {
    constructor() {
       super()
       // default values - before override by attributes
-      this.name = ""
+      this.title = ""
+      this.subtitle = ""
       this.domain = { inf: 0., sup: 100. }
       this.norm = { d1: 40., d2: 60., dt1: 5., dt2: 5., h: 0. }
       this.value = { d1: 50., d2: 50., dt1: 2., dt2: 2., h: 0. }
@@ -155,10 +157,10 @@ export class Gauge extends LitElement {
 
    render() {
       return html`
-         <svg viewBox="-1050 -1050 2100 1500" fill="none" xmlns="http://www.w3.org/2000/svg">
+         <svg viewBox="-1050 -1050 2100 1700" fill="none" xmlns="http://www.w3.org/2000/svg">
 
             <!-- background rectangle with rounded corners -->
-            <rect class="${this.colorClass} light" x="-1050" y="-1050" rx="100" ry="100" width="2100" height="1500" />
+            <rect class="${this.colorClass} light" x="-1050" y="-1050" rx="100" ry="100" width="2100" height="1700" />
 
             <!-- red area, fixed -->
             <path class="red" fill-rule="evenodd" clip-rule="evenodd" d="M 739.593 -693.807 C 859.42 -573.981 947.152 -428.06 997.26 -269.4 L 996.219 -269.06 C 998.694 -258.753 1000 -247.992 1000 -236.928 C 1000 -161.073 938.505 -99.574 862.647 -99.574 C 797.038 -99.574 742.175 -145.576 728.546 -207.082 C 690.725 -316.148 628.521 -416.381 545.344 -499.559 C 400.808 -644.094 204.778 -725.296 0.375 -725.296 C -204.026 -725.296 -400.058 -644.094 -544.593 -499.559 C -627.768 -416.386 -689.968 -316.153 -727.793 -207.083 C -729.748 -198.249 -732.555 -189.736 -736.126 -181.627 C -737.144 -178.332 -738.14 -175.028 -739.106 -171.72 L -740.58 -172.456 C -763.676 -129.095 -809.339 -99.574 -861.893 -99.574 C -937.753 -99.574 -999.245 -161.073 -999.245 -236.928 C -999.245 -243.876 -998.728 -250.705 -997.734 -257.378 L -1000 -258.115 C -950.533 -421.14 -861.5 -571.141 -738.841 -693.807 C -542.786 -889.859 -276.882 -1000 0.375 -1000 C 277.638 -1000 543.541 -889.859 739.593 -693.807 Z" />
@@ -181,9 +183,12 @@ export class Gauge extends LitElement {
             <path fill="black" transform="rotate(${this.valueAngle})" d="M 0 30 L -20 30 A 60 60 0 0 1 -50 0 L ${-this.valueLeftKernelX-20} ${-this.valueLeftKernelY+20} A 60 60 0 0 1 ${-this.valueLeftKernelX} ${-this.valueLeftKernelY} A 900 900 0 0 1 ${-this.valueRightKernelX} ${-this.valueRightKernelY} A 60 60 0 0 1 ${-this.valueRightKernelX+20} ${-this.valueRightKernelY+20} L 50 0 A 60 60 0 0 1 20 30" />
 
             <!-- lower area -->
-            <rect class="${this.colorClass} fairlylight" x="-1050" y="150" rx="100" ry="100" width="2100" height="300" />
-            <text text-anchor="middle" class="text" fill="black" x="0" y="360">
-               ${this.name}
+            <rect class="${this.colorClass} fairlylight" x="-1050" y="150" rx="100" ry="100" width="2100" height="500" />
+            <text text-anchor="middle" class="title" fill="black" x="0" y="360">
+               ${this.title}
+            </text>
+            <text text-anchor="middle" class="subtitle" fill="#666" x="0" y="560">
+               ${this.subtitle}
             </text>
          </svg>
       `
@@ -216,10 +221,14 @@ export class Gauge extends LitElement {
             opacity: 0.1;
          }
 
-         .text {
+         .title {
             font-weight: bold;
             font-size: 200px;
-            ffont-family: sans-serif;
+            font-family: var(--jcb-gauge-font-family, sans-serif);
+         }
+
+         .subtitle {
+            font-size: 150px;
             font-family: var(--jcb-gauge-font-family, sans-serif);
          }
       `
